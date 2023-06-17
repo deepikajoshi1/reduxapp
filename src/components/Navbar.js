64d63e3acf6a19f255634bee32f2904e9857bc43
deepika joshi <upreti.deepika@gmail.com>
2023-06-17 17:47:53 +0800

    created redux store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const amount = useSelector(state => state.amount)
   return (
+
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
@@ -19,7 +22,7 @@ const Navbar = () => {
               </li>
             </ul>
             <div>
-              <button disabled={true} className="btn btn-primary">Your Balance: 10000</button>
+              <button disabled={true} className="btn btn-primary">Your Balance: {amount}</button>
             </div>
 
           </div>
--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,5 @@
-import {applyMiddleware, createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
 import { reducers } from "./reducers";
-
-export const store = createStore(reducers, {}, applyMiddleware(thunk))
+const store = createStore(  reducers,{}, applyMiddleware(thunk));
+export default store;
